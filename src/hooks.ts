256a1994e7d3fbe613ef096c2d82610b67bcefb9
Anwesh43 <anweshthecool0@gmail.com>
2021-04-23 20:49:08 +0530

    addinng util functions

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,8 +1,14 @@
 import {
+    CSSProperties,
     useEffect, useState 
 } from 'react'
 const delay : number = 20 
 const scGap = 0.02 
+
+const sinify : Function = (scale : number) : number => Math.sin(scale * Math.PI)
+const maxScale : Function = (scale : number, i : number, n : number) : number => Math.max(0, scale - i / n)
+const divideScale : Function = (scale : number, i : number, n : number) : number => Math.min(1 / n, maxScale(scale, i, n)) * n 
+
 
 export const useAnimatedScale : Function = () => {
     const [scale, setScale] : [number, Function] = useState(0)
@@ -43,3 +49,4 @@ export const useDimension = () => {
         w, h
     }
 }
+
